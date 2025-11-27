3d2d4406541ace75158d15652996f51e295d06a5
Dipan <dipanmaulick25@gmail.com>
2025-11-27 15:37:30 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import Biryani from "./Biryani"
+
+
+
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return(
+    <>
+      <h1>Hey I am using React</h1>
+      <Biryani/>
+      
+    </>
+    
+  )
+
+}
+
+export default App
---
+++ src/Biryani.jsx
@@ -0,0 +1,8 @@
+function Biryani(){
+    return(
+        <h2>Lets have some biryani</h2>
+
+    )
+}
+
+export default Biryani
