773c2379ae20117e42c1ecdc7dd9de087d4a1582
Dipan <dipanmaulick25@gmail.com>
2025-11-28 16:12:56 +0000

    small change

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 //import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Wish from './Wish.jsx'
+
 
 createRoot(document.getElementById('root')).render(
  
-    <>
+  
     <App/>
-    <Wish/>
-    </>
+   
   
 )
